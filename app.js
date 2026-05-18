199634d1e6f645bbcda7cd5d4e4b96aaaf28db4c
40355116 <mtricklebank01@qub.ac.uk>
2026-05-18 14:27:20 +0100

    Add employee (#3)
    * 2
    
    * stylesheets
    
    * routes
    
    * employee
    
    * employee service
    
    * sdfghjk
    
    * dnfkdjf
    
    * qwertyu
    
    * dnfkdjf
    
    * fixes
    
    * add employee
    
    ---------
    
    Co-authored-by: Emma Sloan <esloan420@qub.ac.uk>

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var employeesRouter = require('./routes/employees');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/employees', employeesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,5 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
