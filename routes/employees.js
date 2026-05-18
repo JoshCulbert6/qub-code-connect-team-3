9dd069c349fe31519fb1fb0297c5c7ade6cf0b87
Millie Tricklebank <mtricklebank01@qub.ac.uk>
2026-05-18 12:05:39 +0100

    employee

--- routes/employees.js
+++ routes/employees.js
@@ -15,3 +15,23 @@ router.post('/add', (req, res) => {
   const createdEmployee = employeeService.createEmployee(newEmployee);
   res.redirect('/employees/' + createdUser.id)
 });
+
+// Read all users
+router.get('/', (req, res) => {
+  const employees = employeeService.getAllEmployees();
+  res.render('employees', { employees: employees })
+});
+
+// Read a user by ID
+router.get('/:id', (req, res) => {
+  const employee = employeeService.getEmployeeById(parseInt(req.params.id));
+  if (!employee) return res.status(404).send('Employee not found');
+  res.render('employee', { employee: employee })
+});
+
+// Update a user by ID form
+router.get('/update/:id', (req, res) => {
+  const employee = employeeService.getEmployeeById(parseInt(req.params.id));
+  if (!employee) return res.status(404).send('Employee not found');
+  res.render('updateEmployee', {employee: employee});
+});
