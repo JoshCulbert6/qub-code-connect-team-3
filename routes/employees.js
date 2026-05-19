016a7b62121c39c94ff62c0d6d66c884eceb43aa
40355116 <mtricklebank01@qub.ac.uk>
2026-05-19 10:20:18 +0100

    Routing (#4)
    * address and drop down
    
    * routing

--- routes/employees.js
+++ routes/employees.js
@@ -4,50 +4,40 @@ var router = express.Router();
 const EmployeeService = require('../services/employeeService');
 const employeeService = new EmployeeService();
 
-//validation
-function validateEmployee(req, res, next) {
-  const { name, position, salary } = req.body;
-
-  if (!name || !position || salary === undefined) {
-    return res.redirect('employees/add?error=required');
-  }
-
-  const parsedSalary = Number(salary);
-  if (isNaN(parsedSalary)) {
-    return res.redirect('employees/add?error=required');
-  }
-
-  if (parsedSalary <= 0) {
-    return res.redirect('employees/add?error=required');
-  }
-
-  req.body.salary = parsedSalary;
-  next();
-};
 
 // Create a new employee form
 router.get('/add', (req, res) => {
   res.render('addEmployee');
 });
 
+//// Read all employees
+router.get("/employees",  async (req, res) =>{
+  const readEmployees = await employeeService.readEmployees();
+  if(!readEmployees) return res.status(404).send('No employees to show');
+  res.render('employees', { employees: readEmployees });
+}); 
+
 // Create a new employee submit
 router.post('/add',  (req, res) => {
   const newEmployee = req.body;
   const createdEmployee = employeeService.createEmployee(newEmployee);
-  res.redirect('/employees/' + createdEmployee.id + '?success=true');
+  res.redirect('/employees');
 });
 
-// Read all employees
-router.get('/', (req, res) => {
-  const employees = employeeService.getAllEmployees();
-  res.render('employees', { employees: employees })
-});
+
 
 // Update a employee by ID form
 router.get('/update/:id',  (req, res) => {
   const employee = employeeService.getEmployeeById(parseInt(req.params.id));
   if (!employee) return res.status(404).send('Employee not found');
   res.render('updateEmployee', {employee: employee});
+});
+
+// Update a user by ID
+router.post('/update/:id', (req, res) => {
+  const updatedEmployee = employeeService.updateEmployee(parseInt(req.params.id), req.body);
+  if (!updatedEmployee) return res.status(404).send('Employee not found');
+  res.redirect('/employees' + updatedEmployee.id)
 });
 
 // Read a employees by ID
@@ -57,6 +47,11 @@ router.get('/:id', (req, res) => {
   res.render('employee', { employee: employee })
 });
 
-
+//delete employee
+router.post('/delete/:id', (req, res) => {
+  const deletedEmployee = employeeService.deleteEmployee(parseInt(req.params.id));
+  if (!deletedEmployee) return res.status(404).send('Employee not found');
+  res.redirect('/employees')
+});
 
 module.exports = router;
